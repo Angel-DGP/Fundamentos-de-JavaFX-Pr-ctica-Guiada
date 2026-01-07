c8223c8dd9d57dbe3671dbbbfac14a02ee9131e5
Angel-DGP <angel.patinogd@gmail.com>
2026-01-06 20:14:58 -0500

    Ejercicio 1

---
+++ src/controlador/Main.java
@@ -0,0 +1,41 @@
+
+package controlador;
+
+import java.io.IOException;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+@Override
+public void start(Stage primaryStage) {
+
+    try {
+        System.out.println(
+            Main.class.getResource("/vista/SumaVista.fxml")
+        );
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(
+            Main.class.getResource("/vista/SumaVista.fxml")
+        );
+
+        Pane ventana = loader.load();
+
+        Scene scene = new Scene(ventana);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+}
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
